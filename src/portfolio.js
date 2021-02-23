eb5c44d35ee105f7023853d9fdcf41329ad66395
Ilya Vlasov <ilyavlasov93@gmail.com>
2021-02-23 13:25:41 -0500

    1st commit
    Portfolio started

---
+++ src/portfolio.js
@@ -0,0 +1,364 @@
+﻿// Summary And Greeting Section
+
+import emoji from "react-easy-emoji";
+
+const illustration = {
+  animated: false // set to true to use active SVG 
+};
+
+const greeting = {
+  username: "Ilya Vlasov",
+  title: "Hi all, I'm Ilya",
+  subTitle: emoji(
+    "A team player with knowledge of HTML, CSS and JavaScript and prior experience in the service industry. Calm under pressure, quick learner with the goal of customer satisfaction."
+  ),
+  resumeLink:
+    "https://drive.google.com/file/d/1m27T1hMv7RsvQBUMu7qP8-xrZsp5zZgL/view",
+  displayGreeting: true // Set false to hide this section, defaults to true
+};
+
+// Social Media Links
+
+const socialMediaLinks = {
+  github: "https://github.com/vlasov-ilya",
+  linkedin: "https://www.linkedin.com/in/ilya-vlasov/",
+  gmail: "ilyavlasov93@gmail.com",
+  instagram: 'https://www.instagram.com/fox20073/',
+  facebook: "https://www.facebook.com/IVV5793",
+  // gitlab: "https://gitlab.com/",
+  // medium: "https://medium.com/",
+  // stackoverflow: "https://stackoverflow.com/",
+  // twitter: 'https://twitter.com/',
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Skills Section
+
+const skillsSection = {
+  title: "What I do",
+  subTitle: "JUNIOR FULL STACK WEB DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK, IMPROVE SKILLS, AND BECOME BETTER  ",
+  skills: [
+    emoji(
+      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+    ),
+    emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
+    emoji(
+      "⚡ Integration of third party services such as Firebase and AWS "
+    )
+  ],
+
+  /* Make Sure to include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "fab fa-html5"
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "fab fa-css3-alt"
+    },
+    {
+      skillName: "sass",
+      fontAwesomeClassname: "fab fa-sass"
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js"
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "fab fa-react"
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "fab fa-node"
+    },
+    {
+      skillName: "swift",
+      fontAwesomeClassname: "fab fa-swift"
+    },
+    {
+      skillName: "mongo-database",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    {
+      skillName: "firebase",
+      fontAwesomeClassname: "fas fa-fire"
+    },
+    {
+      skillName: "angular",
+      fontAwesomeClassname: "fab fa-angular"
+    },
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Education Section
+
+const educationInfo = {
+  display: false, // Set false to hide this section, defaults to true
+  schools: [
+    {
+      schoolName: "Harvard University",
+      logo: require("./assets/images/harvardLogo.png"),
+      subHeader: "Master of Science in Computer Science",
+      duration: "September 2017 - April 2019",
+      desc: "Participated in the research of XXX and published 3 papers.",
+      descBullets: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+      ]
+    },
+    {
+      schoolName: "Stanford University",
+      logo: require("./assets/images/stanfordLogo.png"),
+      subHeader: "Bachelor of Science in Computer Science",
+      duration: "September 2013 - April 2017",
+      desc:
+        "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
+      descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
+    }
+  ]
+};
+
+// My top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "Frontend / Design", 
+      progressPercentage: "60%" 
+    },
+    {
+      Stack: "Backend",
+      progressPercentage: "45%"
+    },
+    {
+      Stack: "Programming",
+      progressPercentage: "50%"
+    }
+  ],
+  displayCodersrank: true // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+};
+
+// Work experience section
+
+const workExperiences = {
+  display: false, //Set it to true to show workExperiences Section
+  experience: [
+    {
+      role: "Software Engineer",
+      company: "Clever Programmer",
+      companylogo: require("./assets/images/cpLogo.png"),
+      date: "September 2020 – Present",
+      desc:
+        "Changing lives of developers one line of code at a time.",
+      descBullets: [
+        "Custom building applications",
+        "Coaching",
+        "Streaming live on YouTube",
+        "Creating tutorials"
+      ]
+    },
+    {
+      role: "Product Owner (PSD2)",
+      company: "Erste Bank",
+      companylogo: require("./assets/images/ersteLogo.jpg"),
+      date: "July 2019 – September 2020",
+      desc:
+        "I lead software developers building the API for the bank in order to fulfil the PSD2 regulation requirements."
+    },
+    {
+      role: "Junior Information Security & Systems Representative",
+      company: "Vodafone",
+      companylogo: require("./assets/images/vodafoneLogo.png"),
+      date: "November 2018 – July 2019",
+      desc:
+        "I've conducted internal audit making sure the organization is compliant with the GDPR."
+    }
+  ]
+};
+
+// /* Your Open Source Section to View Your Github Pinned Projects
+// To know how to get github key look at readme.md */
+
+const openSource = {
+  githubConvertedToken: process.env.bf7810e05d3e5b2543a84c85984e1f9a29d16b60,
+  githubUserName: "vlasov-ilya",
+  showGithubProfile: "true", 
+  display: false 
+};
+
+// Some big projects you have worked on
+
+const bigProjects = {
+  title: "Big Projects",
+  subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
+  projects: [
+    {
+      image: require("./assets/images/saayaHealthLogo.webp"),
+      projectName: "Saayahealth",
+      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "fa"
+        }
+        //  you can add extra buttons here.
+      ]
+    },
+    {
+      image: require("./assets/images/nextuLogo.webp"),
+      projectName: "Nextu",
+      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: ""
+        }
+      ]
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Achievement Section
+// Include certificates, talks etc
+
+const achievementSection = {
+  title: emoji("Achievements And Certifications 🏆 "),
+  subtitle:
+    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+
+  achievementsCards: [
+    {
+      title: "Google Code-In Finalist",
+      subtitle:
+        "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
+      image: require("./assets/images/codeInLogo.webp"),
+      footerLink: [
+        {
+          name: "Certification",
+          url:
+            ""
+        },
+        {
+          name: "Award Letter",
+          url:
+            ""
+        },
+        {
+          name: "Google Code-in Blog",
+          url:
+            ""
+        }
+      ]
+    },
+    {
+      title: "Google Assistant Action",
+      subtitle:
+        "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
+      image: require("./assets/images/googleAssistantLogo.webp"),
+      footerLink: [
+        {
+          name: "View Google Assistant Action",
+          url:
+            ""
+        }
+      ]
+    },
+
+    {
+      title: "PWA Web App Developer",
+      subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
+      image: require("./assets/images/pwaLogo.webp"),
+      footerLink: [
+        { name: "Certification", url: "" },
+        {
+          name: "Final Project",
+          url: ""
+        }
+      ]
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Blogs Section
+
+const blogSection = {
+  title: "Blogs",
+  subtitle:
+    "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+
+  blogs: [
+  ],
+  display: false // Set true to show this section, defaults to false
+};
+
+// Talks Sections
+
+const talkSection = {
+  title: "TALKS",
+  subtitle: emoji(
+    ""
+  ),
+
+  talks: [
+    {
+      title: "",
+      subtitle: "",
+      slides_url: "",
+      event_url: ""
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Podcast Section
+
+const podcastSection = {
+  title: emoji("Podcast 🎙️"),
+  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
+
+  // Please Provide with Your Podcast embeded Link
+  podcast: [
+    ""
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+const contactInfo = {
+  title: emoji("Contact Me ☎️"),
+  subtitle:
+    "Contact me at anytime!",
+  number: "(617) 599 8604",
+  email_address: "ilyavlasov93@gmail.com"
+};
+
+// Twitter Section
+
+const twitterDetails = {
+  userName: "", 
+  display: false // Set true to display this section, defaults to false
+};
+
+export {
+  illustration,
+  greeting,
+  socialMediaLinks,
+  skillsSection,
+  educationInfo,
+  techStack,
+  workExperiences,
+  openSource,
+  bigProjects,
+  achievementSection,
+  blogSection,
+  talkSection,
+  podcastSection,
+  contactInfo,
+  twitterDetails
+};
